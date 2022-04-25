a077a0da3c6c5d25a95cf70fb554ef867bf9c70a
Fayez <fayezkhan680@gmail.com>
2022-04-25 19:58:49 +0530

    commit on 'main'

---
+++ main.js
@@ -0,0 +1,26 @@
+const txtBox=document.getElementById("txt-box")
+const btn=document.getElementById("btn")
+
+btn.addEventListener('click',function(){
+    let speech = true
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition()
+    recognition.interimResults= true
+    recognition.addEventListener('result', e=>{
+        const transcript = Array.from(e.results)
+        .map(result =>result[0])
+        .map(result =>result.transcript)
+        
+
+        txtBox.innerHTML = transcript;
+    })
+    // setTimeout(() => {
+    //     recognition.start();
+    //   }, 100000000);
+      recognition.onspeechend = function() {
+        recognition.stop();
+      }
+    if(speech==true){
+        recognition.start()
+    }
+})
